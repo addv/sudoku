cebe6f320ff0c4c336ba67e78aae5d31bf291f4b
AddV <1786276856@qq.com>
2012-07-04 11:08:59 +0800

    cancel the test data

--- shudu.c
+++ shudu.c
@@ -21,6 +21,7 @@ void print_result(const char arrS[][9], const char arrR[][9]);
 
 int showNum, currNum = 0;
 char arrShu[9][9] = {
+	/*
 	1, 2, 3, 0, 0, 0, 0, 0, 0, 
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 
@@ -30,8 +31,8 @@ char arrShu[9][9] = {
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 
 	0, 0, 0, 0, 0, 0, 0, 0, 0
-};// Placed outside in order to can be invoked when more than one answer.
-char arrShu2[9][9] = {
+};
+char arrShu2[9][9] = {//*/
 	8, 0, 0, 0, 0, 0, 0, 0, 0, 
 	0, 0, 3, 6, 0, 0, 0, 0, 0, 
 	0, 7, 0, 0, 9, 0, 2, 0, 0, 
