eb2acda2ff3b3a616fc8eca2526791d8c1512419
AddV <1786276856@qq.com>
2012-07-04 11:40:13 +0800

    optimize storage

--- shudu.c
+++ shudu.c
@@ -14,13 +14,14 @@
 #include <conio.h>
 #include <memory.h>
 
-int check_no_conflict(const char* const arr, const char* const off, char num);
-int test(char arr[]);
-void print_start(char const arr[][9]);
-void print_result(const char arrS[][9], const char arrR[][9]);
+int check_no_conflict(const int* const arr, const int* const off, int num);
+int test(int arr[]);
+void print_start(int const arr[][9]);
+void print_result(const int arrS[][9], const int arrR[][9]);
+int* memchr_int(const void* buf, int c, size_t count);
 
 int showNum, currNum = 0;
-char arrShu[9][9] = {
+int arrShu[9][9] = {
 	8, 0, 0, 0, 0, 0, 0, 0, 0, 
 	0, 0, 3, 6, 0, 0, 0, 0, 0, 
 	0, 7, 0, 0, 9, 0, 2, 0, 0, 
@@ -31,7 +32,7 @@ char arrShu[9][9] = {
 	0, 0, 8, 5, 0, 0, 0, 1, 0, 
 	0, 9, 0, 0, 0, 0, 4, 0, 0
 };// Placed outside in order to can be invoked when more than one answer.
-char arrR[9][9] = {0};
+int arrR[9][9] = {0};
 
 int main(int argc, char* argv[])
 {
@@ -52,7 +53,7 @@ int main(int argc, char* argv[])
 	}
 	printf("\nStart!\n");
 	
-	test((char *)arrShu);
+	test((int *)arrShu);
 	
 	if (showNum == 1)
 	{
@@ -67,21 +68,20 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-int test(char arr[])
+int test(int arr[])
 {
 	int result;
 	// 1: found answer
 	// 0: conflict or no found
-	char* p;
-	char* p0;
-	char num;
+	int p[81];
+	int* p0;
+	int num;
 	int next;
 	
-	p = malloc(81);
-	memcpy(p, arr, 81);
+	memcpy(p, arr, 81 * sizeof(int));
 	
 	// find out whether it has been filled
-	p0 = memchr(p, 0, 81);
+	p0 = memchr_int(p, 0, 81);
 	if (p0)
 	{
 		result = 0;// did not find at the beginning
@@ -114,7 +114,7 @@ int test(char arr[])
 	}
 	else
 	{// fill to full
-		memcpy(arrR, p, 81);// Can be written back to arr
+		memcpy(arrR, p, 81 * sizeof(int));// Can be written back to arr
 		result = 1;
 		if (showNum != 1)
 		{
@@ -123,11 +123,10 @@ int test(char arr[])
 		}
 	}
 	
-	free(p);
 	return result;
 }
 
-int check_no_conflict(const char* const arr, const char* const off, char num)
+int check_no_conflict(const int* const arr, const int* const off, int num)
 {
 	int row = (off - arr) / 9;
 	int col = (off - arr) % 9;
@@ -151,10 +150,10 @@ int check_no_conflict(const char* const arr, const char* const off, char num)
 	return r;
 }
 
-void print_start(const char arr[][9])
+void print_start(const int arr[][9])
 {
 	int i, j;
-	char c;
+	int c;
 	for (i = 0; i < 9; i++)
 	{
 		for (j = 0; j < 9; j++)
@@ -173,11 +172,11 @@ void print_start(const char arr[][9])
 	}
 }
 
-void print_result(const char arrS[][9], const char arrR[][9])
+void print_result(const int arrS[][9], const int arrR[][9])
 {
 	int i, j;
 	// having space not be fill in numbers means traverse is end
-	if (memchr(arrR, 0, 81))
+	if (memchr_int(arrR, 0, 81))
 	{
 		printf("There is no result yet.\n");
 	}
@@ -201,3 +200,19 @@ void print_result(const char arrS[][9], const char arrR[][9])
 	}
 }
 
+int* memchr_int(const void* buf, int c, size_t count)
+{
+	int* p;
+	size_t i;
+
+	p = NULL;
+	for (i = 0; i < count; i++)
+	{
+		if (*((int *)buf + i) == c)
+		{
+			p = (int *)buf + i;
+			break;
+		}
+	}
+	return p;
+}
